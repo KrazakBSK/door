463700d52f5c3cc4a050fb2818aef770db62db62
krazakbsk@gmail.com <krazakbsk@gmail.com>
2021-01-04 09:03:03 -0800

    added door main file

---
+++ door.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+
+
+void displayPrompt(){
+	 cout << "Welcome to Door!" << endl;
+	 cout << "Simply choose a number associated with a door." << endl;
+         cout << "Survive as long as you can :)." << endl;
+	 cout << "Good Luck!" << endl;
+}
+
+int main(){
+	displayPrompt();
+	return 0;
+}
